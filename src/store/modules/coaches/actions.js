ec0644fa7ad429be3414860b3ea457d9d4e2317f
biruk <birukmolla30@gmail.com>
2021-04-24 11:23:34 -1200

    store coach data to the firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,14 +1,24 @@
 export default {
-    registerCoach(context, data){
+   async registerCoach(context, data){
+        const coachId = context.rootGetters.userId;
         const coachData = {
-            id: 'c3',
             firstName: data.first,
             lastName: data.last,
             description: data.desc,
             areas: data.areas,
             hourlyRate: data.rate
         }
-
-        context.commit('registerCoach', coachData);
+       const response = await fetch(`https://vue-http-demo-658a5-default-rtdb.firebaseio.com/coaches/${coachId}.json`, {
+            method: 'PUT',
+            body: JSON.stringify(coachData)
+        });
+        // const responseData = await response.json();
+        if(!response.ok){
+            // error
+        }
+        context.commit('registerCoach',{
+            ...coachData, 
+            id: coachId
+        });
     }
 };
